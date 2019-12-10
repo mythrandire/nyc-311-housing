6c5fc1a730eb6ba1e983bc970376d903014ef199
mythrandire <39613187+mythrandire@users.noreply.github.com>
2019-12-10 15:29:37 -0500

    filtering 311 into dataframe

---
+++ pandas_311.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sodapy import Socrata
+
+client = Socrata("data.cityofnewyork.us", "PXkXxMVDwDUe5OaptO5qTbo2e",
+                 username="dso2119@columbia.edu", password="Providence@024")
+
+
+client.timeout = 500
+results = client.get("erm2-nwe9", limit=500000)
+raw_311_df = pd.DataFrame.from_records(results)
+zip_311_df = raw_311_df.filter(items=['created_date', 'complaint_type', 'incident_zip', 'status'])
+no_nan_df = zip_311_df.dropna()
+no_nan_df.head(20000000)
